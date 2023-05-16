84fe480959c34d9adaf909bed7c11e737ba14bbd
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2023-05-15 18:23:53 -0700

    blarg

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -47,13 +47,21 @@ public class _01_StringMethods {
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-    	String sub = "underscores";
-         String removedSubstring = s.replace(sub, "");
-        int  num = (s.length() - removedSubstring.length()) / sub.length();
-    	if(num >= 1) {
-    		s.replace(' ', '_');
+    	String sub = "underscores";    
+    	int num = 0;
+            int index = s.indexOf(sub);
+            while( index != -1 ) {
+                num++;
+                index = s.indexOf(sub, index + sub.length());
+            }
+        if(num > 0) {
+    		String p = s.replace(" ", "_");
+    		 return p;
     	}
-        return s;
+        else {
+        return s;	
+        }
+       
     }
 
     // Return the name of the person whose LAST name would appear first if they
@@ -61,60 +69,82 @@ public class _01_StringMethods {
     // You cannot assume there are no extra spaces around the name, but you can
     // assume there is only one space between the first and last name
     public static String lineLeader(String s1, String s2, String s3) {
-       s1.trim();
-       s2.trim();
-       s3.trim();
-       int n1 =s1.indexOf(' ');
-       int n2 =s2.indexOf(' ');
-       int n3 =s3.indexOf(' ');
-    	char c1 = s1.charAt(n1+1);
-        char c2 = s2.charAt(n2+1);
-        char c3 = s3.charAt(n3+1);
+      String t1 = s1.trim();
+      String t2 =  s2.trim();
+      String t3 = s3.trim();
+       int n1 =t1.indexOf(' ');
+       int n2 =t2.indexOf(' ');
+       int n3 =t3.indexOf(' ');
+    	char c1 = t1.charAt(n1+1);
+        char c2 = t2.charAt(n2+1);
+        char c3 = t3.charAt(n3+1);
         if(c1<c2 && c1<c3){
-        	return s1;
+        	String q1=s1;
+        	return q1;
         }
         else if(c2<c1 && c2<c3){
-        	return s2;
+        	String q2=s2;
+        	return q2;
         }
-    	return s3;
+        String q3=s3;
+    	return q3;
     }
 
     // Return the sum of all numerical digits in the String
     public static int numeralSum(String s) {
     	int num = 0;
-    	for (int j = 0; j < s.length(); j++) {
-    		if (Character.isDigit(s.charAt(j))) {
-    			char[] strChars = new char[1];
-    			strChars[0]=s.charAt(j);
-    			String q1 = Arrays.toString(strChars);
-    			int q2 = Integer.parseInt(q1);
-    			num+=q2;
-    		}
-    		
+    	for (int j = 0; j < 10; j++) {
+			String substring = Integer.toString(j);;
+	    	int numOccurances = 0;
+	        int index = s.indexOf(substring);
+	        while( index != -1 ) {
+	            numOccurances++;
+	            index = s.indexOf(substring, index + substring.length());
+	        }	
+	        num+=(numOccurances*j);
 		}
+    	
         return num;
     }
 
     // Return the number of times String substring appears in String s
     public static int substringCount(String s, String substring) {
-        return 0;
+           int numOccurances = 0;
+           int index = s.indexOf(substring);
+           while( index != -1 ) {
+               numOccurances++;
+               index = s.indexOf(substring, index + substring.length());
+           }
+        return numOccurances;
     }
 
     // Call Utilities.encrypt at the bottom of this file to encrypt String s
     public static String encrypt(String s, char key) {
-        return null;
+    	byte[] b = s.getBytes();
+    	byte b2 = (byte) key;
+    	String q = Utilities.encrypt(b, b2);
+        return q;
     }
 
     // Call Utilities.decrypt at the bottom of this file to decrypt the
     // cyphertext (encrypted text)
     public static String decrypt(String s, char key) {
-        return null;
+    	byte b2 = (byte) key;
+    	String q = Utilities.decrypt(s, b2);
+        return q;
     }
 
     // Return the number of words in String s that end with String substring
     // You can assume there are no punctuation marks between words
     public static int wordsEndsWithSubstring(String s, String substring) {
-        return 0;
+        String sub = substring + " ";    
+    	int numOccurances = 0;
+            int index = s.indexOf(sub);
+            while( index != -1 ) {
+                numOccurances++;
+                index = s.indexOf(sub, index + sub.length());
+            }
+			return numOccurances;
     }
 
     // Given String s, return the number of characters between the first
@@ -128,7 +158,21 @@ public class _01_StringMethods {
     // palindromes are words or phrases are read the same forward as backward.
     // HINT: ignore/remove all punctuation and spaces in the String
     public static boolean palindrome(String s) {
+    	s.trim();
+    	s.replace(" ", "");
+    	s.replace(",", "");
+    	s.replace(".", "");
+    	s.replace("!", "");
+    	s.replace("?", "");
+    	s.replace("-", "");
+    	s.replace(":", "");
+    	s.toLowerCase();
+    	String sub1 = s.substring(0, s.length()/2);
+    	String sub2 = s.substring(s.length()/2, s.length());
+    	if(sub1==sub2) {
         return true;
+    	}
+		return false;
     }
 }
 
